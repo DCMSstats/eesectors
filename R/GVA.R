c31eca90cbbe45ccedb94272c1afab6fba5fae45
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2016-11-23 16:24:36 +0000

    Added test data
    In advance of getting access to the underlying data, add datasets from the original 2016 report to give soemthign to develop against, and to provide test cases once the underlying data is available.

---
+++ R/GVA.R
@@ -0,0 +1,14 @@
+#' GVA data for 2010:2015.
+#'
+#' Data extracted from the 2016 Economic Estimates of DCMS Sectors 2016 report.
+#' These data are provided for testing ouputs of this package against, and for
+#' the initial development phase during which the original data sources were not
+#' available.
+#'
+#' @format A data frame with 48 rows and 3 variables: \describe{
+#'   \item{sector}{DCMS sector} \item{year}{calendar year} \item{GVA}{gross
+#'   value added} }
+#' @source
+#'   \url{https://www.gov.uk/government/statistics/dcms-sectors-economic-estimates-2016}
+#'
+"GVA"
