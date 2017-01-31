890df4cc2fed20e5a4f880c102c0c24931fc116a
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-01-31 15:29:52 +0000

    Add end-to-end test for extract_DCMS_sectors

--- tests/testthat/test_extract_DCMS_data.R
+++ tests/testthat/test_extract_DCMS_data.R
@@ -1,12 +1,25 @@
 context("extract_DCMS_sectors works as expected")
 
-test_that(
-  "extract_DCMS_sectors can extract data from a dummy spreadsheet",
-  {
-    skip('Tests not yet implemented')
+testxl <- file.path('testdata', 'DCMS_sectors_test.xlsm')
+output <- file.path('testdata', 'DCMS_sectors.Rds')
 
-    extract_DCMS_sectors('~/Documents/eesectors_data/dcms/OFFICIAL_working_file_dcms_V13.xlsm')
-    expect_true(file.exists('./DCMS_sectors.Rds'))
+test_that(
+  "extract_ABS_data can extract data from a dummy spreadsheet",
+  {
+
+    expect_message(extract_DCMS_sectors(testxl, sheet_name = 'DCMS_sectors_test', skip = 7, output_path = 'testdata'))
+    expect_true(file.exists(output))
+
+    DCMS_sectors_output <- readRDS(output)
+    DCMS_sectors_output$SIC <- as.character(DCMS_sectors_output$SIC)
+
+    expect_is(DCMS_sectors_output, c('data.frame', 'tbl', 'tbl_df'))
+
+    # Use the built in DCMS_sectors dataset
+
+    expect_identical(DCMS_sectors_output, DCMS_sectors)
 
   }
 )
+
+file.remove(output)
