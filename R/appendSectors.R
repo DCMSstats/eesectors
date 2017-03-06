a47120e8a7527417d4d541c5fec606c05807f485
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-03-06 17:05:51 +0000

    Re-factored part of appendSectors.R function

--- R/appendSectors.R
+++ R/appendSectors.R
@@ -1,25 +1,31 @@
 #' @title Append sector membership to APS data extract
 #'
 #' @description This function appends DCMS sector membership to an APS
-#' SPSS-extracted data frame.
+#'   SPSS-extracted data frame.
 #'
 #' @details The function \code{eesectors::extract_APS_data} extracts APS data
-#' from an APS SPSS file. This function appends DCMS sector membership to an APS
-#' SPSS-extracted data frame before further analysis, on a first job and second job basis
+#'   from an APS SPSS file. This function appends DCMS sector membership to an
+#'   APS SPSS-extracted data frame before further analysis, on a first job and
+#'   second job basis
 #'
 #'   1. The function converts the character vector DCMS_sectors$SIC to double
 #'
-#'   2. The function loops through the sectors found in DCMS_sectors, and uses DCMS_sectors
-#'   as a lookup for membership of each person's main and second job in each sector
+#'   2. The function loops through the sectors found in DCMS_sectors, and uses
+#'   DCMS_sectors as a lookup for membership of each person's main and second
+#'   job in each sector
 #'
 #'   3. The function adds membership in the form of two variables per sector
 #'
 #'   4. The dataframe with +2*n_sectors variables is returned
 #'
-#' @param x a data frame as extracted by \code{eesectors::extract_APS_data}
+#' @param x a `data.frame` as extracted by \code{eesectors::extract_APS_data}
+#' @param sectors a `data.frame` as extracted by
+#'   \code{eesectors::extract_DCMS_sectors}. This defaults to the `DCMS_sectors`
+#'   object which is the `extract_DCMS_sectors` function applied to the 2016
+#'   data.
 #'
-#' @return The function returns the APS data as a dataframe with the sector membership
-#' variables appended
+#' @return The function returns the APS data as a dataframe with the sector
+#'   membership variables appended
 #'
 #' @examples
 #'
@@ -32,32 +38,29 @@
 #'
 #' @export
 
-appendSectors <- function(x){
+appendSectors <- function(x, sectors = eesectors::DCMS_sectors){
 
   # Get the SICs into the right format
-  DCMS_sectors2 <- DCMS_sectors
-  DCMS_sectors2$SIC<-round(as.numeric(DCMS_sectors$SIC)*100)
+  sectors$SIC_long <- elongate_SIC(sectors$SIC)
   #DCMS_sectors2$SIC<-as.numeric(gsub("[.]", "", DCMS_sectors$SIC))
 
   # Find out how many sectors there are
-  sectors <- unique(DCMS_sectors$sector)
-
-  # Create an new data frame with the sectors
-  APS_data_sectors <- x
+  unique_sectors <- unique(sectors$sector)
 
   # Loop for each sector in sectors
-  for (s in sectors){
-    APS_data_sectors[paste0(s,"_main")] <- FALSE
-    APS_data_sectors[paste0(s,"_second")] <- FALSE
+  for (s in unique_sectors){
+    x[paste0(s,"_main")] <- FALSE
+    x[paste0(s,"_second")] <- FALSE
 
-    # Find the SIC codes that define the sector
-    # (where DCMC sectors is =s, and then where that SIC is TRUEly present in that sector)
-    SIC=DCMS_sectors2$SIC[DCMS_sectors$sector==s][DCMS_sectors$present[DCMS_sectors$sector==s]==TRUE]
+    # Find the SIC codes that define the sector (where DCMC sectors is =s, and
+    # then where that SIC is TRUEly present in that sector)
+    SIC <- sectors$SIC_long[eesectors::DCMS_sectors$sector==s][eesectors::DCMS_sectors$present[eesectors::DCMS_sectors$sector==s]==TRUE]
 
-    # If a person has a job in that sector, per sector, per main/2nd job then "TRUE"
-    APS_data_sectors[paste0(s,"_main")]<-APS_data_sectors$INDC07M %in% SIC
-    APS_data_sectors[paste0(s,"_second")]<-APS_data_sectors$INDC07S %in% SIC
+    # If a person has a job in that sector, per sector, per main/2nd job then
+    # "TRUE"
+    x[paste0(s,"_main")] <- x$INDC07M %in% SIC
+    x[paste0(s,"_second")] <- x$INDC07S %in% SIC
   }
-  return(APS_data_sectors)
+  return(x)
 }
 
