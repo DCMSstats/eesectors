b90c9736e2089017d3cf88573e812e9ac4c4ee83
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-03-06 16:53:59 +0000

    Add utils function to elongate SIC

--- tests/testthat/test_appendSectors.R
+++ tests/testthat/test_appendSectors.R
@@ -8,13 +8,13 @@ test_that(
     #test that the output spss file matches the expected format
     APS_reference <- readRDS(reference)
     APS_sectors <- appendSectors(APS_reference)
-    nsectors=length(unique(DCMS_sectors$sector))
+    nsectors <- length(unique(DCMS_sectors$sector))
 
-    expect_equal(length(APS_sectors),length(APS_reference)+2*nsectors)
+    expect_equal(length(APS_sectors), length(APS_reference) + 2 * nsectors)
 
     #test that each variable contains expected data type (logical)
     for(s in unique(DCMS_sectors$sector)){
-      expect_type(eval(parse(text=paste0("APS_sectors$",s,"_main"))),"logical")
+      expect_type(eval(parse(text=paste0("APS_sectors$", s,"_main"))),"logical")
       }
   }
 )
